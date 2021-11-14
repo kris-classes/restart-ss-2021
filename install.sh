e4ff637524961ee0c851b9de3a6137d4873b1791
Kris Pritchard <2504972+krp@users.noreply.github.com>
2021-11-15 10:39:33 +1300

    Added micro text editor to install script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 #run this with the command :
 # curl https://raw.githubusercontent.com/kris-classes/restart/main/install.sh | bash
 echo "Starting restart install script"
+echo "============"
+
 echo "installing cowsay"
 sudo yum install -y cowsay
 echo "finished installing cowsay" | cowsay
@@ -13,3 +15,7 @@ echo "finished installing tmux"
 echo "installing net-tools"
 sudo yum install -y net-tools
 echo "finished installing net-tools"
+
+echo "installing micro text editor"
+curl https://getmic.ro | bash
+echo "finished installing micro text editor"
