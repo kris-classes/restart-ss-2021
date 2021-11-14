94d87b05f86b3946a7923fde3a65ca99255bad22
Kris Pritchard <2504972+krp@users.noreply.github.com>
2021-11-15 10:49:13 +1300

    Update PATH with our own bin directory

--- install.sh
+++ install.sh
@@ -22,4 +22,12 @@ echo "finished installing sl"
 
 echo "installing micro text editor"
 curl https://getmic.ro | bash
+cd
+mkdir bin
+mv micro bin/
 echo "finished installing micro text editor"
+
+
+echo "Update .bashrc with /bin for our PATH"
+echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bashrc"
+echo "Done updating PATH"
