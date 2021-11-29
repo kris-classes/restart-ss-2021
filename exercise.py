4d46caccc55e24e884e2b7655267b05179032313
MorganAWS <93165153+MorganAWS@users.noreply.github.com>
2021-11-29 13:23:25 +1300

    Creating exercise.py
    The start of the group project

---
+++ exercise.py
@@ -0,0 +1,43 @@
+'''
+Password strength as a Service
+PSaaS
+
+A user can send a password and we tell them whether it is a good password or not.
+User can also send a word and we give them the MD5 hash.
+
+'''
+
+import hashlib
+import socket
+import random
+import time
+
+# Create socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind socket
+server_socket.bind(("localhost", 12345))
+
+# Listen on the port
+server_socket.listen()
+
+# Clients must send data
+print("Waiting for connection...")
+
+# Accept connection from client
+(client, addr) = server_socket.accept()
+print("Client connected\n", addr)
+
+# Receive data
+input_data = client.recv(1024)
+  
+# Hash the data using MD5
+# Send the response back
+client.send(b'input data received')
+
+# Close the client socket
+client.close()
+# Close the server socket
+server_socket.close()
+
+# End the program
